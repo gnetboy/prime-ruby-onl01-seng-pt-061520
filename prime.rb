2bcd799264e55cfc23f123f9fcd346d43bfbdf02
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-23 06:01:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -6,6 +6,12 @@
   #end
    # true
  #end
+ 
+ require 'benchmark'
+require 'bigdecimal/math'
+
+# calculate pi to 10k digits
+puts Benchmark.measure { BigMath.PI(10_000) }
   def prime?(num)
     return false if num <=1
     (2..(num - 1)).each do |n|
