b8c7efc46c336546d1f33f8bc1f2b1085ff58764
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-23 05:47:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+#def prime?(num)
+ #  return false if num <=1
+  # for i in 2..num-1
+   #return false if num % i==0
+  #end
+   # true
+ #end
+  def prime?(num)
+    return false if num <=1
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+  end
+  
